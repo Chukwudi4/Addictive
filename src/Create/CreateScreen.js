3302335edc8607dcb53cfbf03cf03a620180d847
Ezichi Chukwudi <chukwudiezichi@gmail.com>
2020-11-27 12:47:05 +0100

    add modal

--- src/Create/CreateScreen.js
+++ src/Create/CreateScreen.js
@@ -1,6 +1,14 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import React, { useLayoutEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+  TextInput,
+  Modal,
+} from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import { widthPercentageToDP as w } from 'react-native-responsive-screen';
 import UUIDGenerator from 'react-native-uuid-generator';
@@ -10,31 +18,33 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 import moment from 'moment';
 
 export function CreateScreen({ navigation, route }) {
-  const [selected, setSelected] = useState(0);
-  const [date, setDate] = useState(new Date());
-  const [showDate, setShowDate] = useState(false);
-  const items = [
+  const data = [
     'Alcohol',
     'Pornography and Masturbation',
     'Substance Use',
     'Social Media',
     'Marijuana',
   ];
+
+  const [selected, setSelected] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [showDate, setShowDate] = useState(false);
+  const [custom, setCustom] = useState('');
+  const [visible, setVisible] = useState(false);
+  const [items, setItems] = useState(data);
   const addictions = route.params?.addictions ?? [];
   const dispatch = useDispatch();
 
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => (
-        <Text
-          style={{
-            fontSize: w(3.5),
-            marginHorizontal: w(2),
-            color: '#fff',
-            fontWeight: 'bold',
-          }}
-          onPress={save}>
+        <Text style={styles.headerButton} onPress={save}>
           Done
+        </Text>
+      ),
+      headerLeft: () => (
+        <Text style={styles.headerButton} onPress={() => setVisible(true)}>
+          Other
         </Text>
       ),
       headerTitle: '',
@@ -91,6 +101,44 @@ export function CreateScreen({ navigation, route }) {
 
   return (
     <View style={styles.container}>
+      <Modal
+        style={{ alignItems: 'center', justifyContent: 'center' }}
+        transparent
+        visible={visible}>
+        <View style={styles.modalView}>
+          <TextInput
+            style={styles.customInput}
+            placeholder="Opiod"
+            onChangeText={(text) => setCustom(text)}
+            value={custom}
+          />
+          <View
+            style={{
+              justifyContent: 'space-between',
+              width: w(40),
+              flexDirection: 'row',
+            }}>
+            <Text
+              onPress={() => setVisible(false)}
+              style={styles.selectionButton}>
+              Cancel
+            </Text>
+            <Text
+              onPress={() => {
+                if (!custom) {
+                  return;
+                }
+                items.push(custom);
+                setSelected(items.length - 1);
+                setItems(items);
+                setVisible(false);
+              }}
+              style={styles.selectionButton}>
+              Done
+            </Text>
+          </View>
+        </View>
+      </Modal>
       {showDate && (
         <DateTimePicker
           minimumDate={new Date()}
@@ -117,9 +165,42 @@ const styles = StyleSheet.create({
     backgroundColor: '#4c669f',
     flex: 1,
   },
+  customInput: {
+    borderBottomColor: '#4c669f',
+    borderBottomWidth: w(0.5),
+    color: '#4c669f',
+    padding: 0,
+    width: w(40),
+    marginBottom: w(4),
+  },
   buttonText: {
     color: '#fff',
     fontSize: w(5),
+    fontWeight: 'bold',
+  },
+  selectionButton: {
+    padding: w(4),
+    paddingTop: w(2),
+    borderRadius: w(3),
+    textAlignVertical: 'center',
+    textAlign: 'center',
+    backgroundColor: '#4c669f',
+    color: '#fff',
+    fontSize: w(3.5),
+    fontWeight: 'bold',
+  },
+  modalView: {
+    width: w(90),
+    height: w(70),
+    backgroundColor: '#fff',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerButton: {
+    fontSize: w(3.5),
+    marginHorizontal: w(2),
+    color: '#fff',
     fontWeight: 'bold',
   },
 });
