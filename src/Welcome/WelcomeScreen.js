b60c17880391ba1fd571505b92e2d05c71622457
Ezichi Chukwudi <chukwudiezichi@gmail.com>
2020-11-27 13:01:43 +0100

    create splash screen

--- src/Welcome/WelcomeScreen.js
+++ src/Welcome/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import { useDispatch } from 'react-redux';
 import { setAddictions } from '../../redux/actions';
@@ -23,8 +23,15 @@ export default function WelcomeScreen({ navigation }) {
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome</Text>
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/splash.png')}
+        style={styles.container}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+});
